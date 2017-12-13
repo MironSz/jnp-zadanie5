0573cdb25e8569f09b3ebf1790f0bdad1fe51c25
miron <miron.szewczyk@autonomik.pl>
2017-12-13 12:39:10 +0100

    Nie dziala pop na kopi

--- keyed_queue.h
+++ keyed_queue.h
@@ -17,42 +17,41 @@ class lookup_error : public std::exception {
 template<class K, class V>
 class keyed_queue {
 private:
-
     struct cmp {
-        bool operator() (std::shared_ptr<K> k1, std::shared_ptr<K> k2) {
+        bool operator()(std::shared_ptr <K> k1, std::shared_ptr <K> k2) {
             return *k1 < *k2;
         }
     };
 
-    using pairKV = std::pair<std::shared_ptr<K>, std::shared_ptr<V>>;
+    using pairKV = std::pair <std::shared_ptr<K>, std::shared_ptr<V>>;
     using itKV = typename std::list<pairKV>::iterator;
-    using list_of_itarator = std::list<itKV>;
-    using map_key_to_list_of_occurances  = std::map <std::shared_ptr<K>, list_of_itarator, cmp>;
-    bool shallow_copy_enable;
-    std::shared_ptr<std::list<pairKV>>  list_of_pairs;
+    using list_of_iterators = std::list<itKV>;
+    using map_key_to_list_of_occurances  = std::map <std::shared_ptr<K>, list_of_iterators, cmp>;
 
-    std::shared_ptr<map_key_to_list_of_occurances> map_of_iterators;
+    bool shallow_copy_enable = true;
 
-    void full_copy(){
+    std::shared_ptr <std::list<pairKV>> list_of_pairs;
+    std::shared_ptr <map_key_to_list_of_occurances> map_of_iterators;
 
-        auto new_list = std::list<pairKV>(*list_of_pairs.get());
+    void full_copy() {
+        auto new_list = std::list<pairKV>(*list_of_pairs);
 
         map_key_to_list_of_occurances new_map;
 
-        for(itKV it = new_list.begin();it != new_list.end();++it){
-            if(new_map.find(it->first) == new_map.end()){
-                new_map.insert(std::make_pair(it->first,list_of_itarator{}));
+        for (itKV it = new_list.begin(); it != new_list.end(); ++it) {
+            if (new_map.find(it->first) == new_map.end()) {
+                new_map.insert(std::make_pair(it->first, list_of_iterators{}));
             }
             new_map[it->first].push_back(it);
         }
-
-        list_of_pairs = std::make_shared<std::list<pairKV>>(new_list);
+        //TODO napisz najpierw stworzenie shared_ptr potem przypisz
+        list_of_pairs = std::make_shared < std::list < pairKV >> (new_list);
         map_of_iterators = std::make_shared<map_key_to_list_of_occurances>(new_map);
     }
 
     class k_iterator {
     private:
-        typename map_key_to_list_of_occurances ::const_iterator i;
+        typename map_key_to_list_of_occurances::const_iterator i;
 
     public:
         k_iterator(typename map_key_to_list_of_occurances::const_iterator it) : i(it) {};
@@ -85,87 +84,79 @@ public:
     }
 
     keyed_queue() {
-        list_of_pairs = std::make_shared<std::list<pairKV>>(std::list<pairKV>{});
-        map_of_iterators = std::make_shared<map_key_to_list_of_occurances>(map_key_to_list_of_occurances{});
+        auto new_list = std::make_shared < std::list < pairKV >> (std::list < pairKV > {});
+        auto new_map = std::make_shared<map_key_to_list_of_occurances>(map_key_to_list_of_occurances{});
+        list_of_pairs = new_list;
+        map_of_iterators = new_map;
         shallow_copy_enable = true;
         //TODO licznik
     }
 
     keyed_queue(keyed_queue const &old_queue) {
-        list_of_pairs = old_queue.list_of_pairs;
-        map_of_iterators = old_queue.map_of_iterators;
+        keyed_queue new_queue;
+        new_queue.list_of_pairs = old_queue.list_of_pairs;
+        new_queue.map_of_iterators = old_queue.map_of_iterators;
+        std::cout <<"Nowa kopia count:" <<old_queue.list_of_pairs.use_count()<<"\n";
         //TODO licznik
-        if(shallow_copy_enable == false){
-            full_copy();
+        if (shallow_copy_enable == false) {
+            std::cout << "w srdku\n";
+            new_queue.full_copy();
         }
+        list_of_pairs = new_queue.list_of_pairs;
+        map_of_iterators = new_queue.map_of_iterators;
+
         shallow_copy_enable = true;
     }
 
     keyed_queue(keyed_queue &&) = default;
 
     void push(K const &k, V const &v) {
-        std::shared_ptr<K> k_ptr = std::make_shared<K>(k);//
+        std::shared_ptr <K> k_ptr = std::make_shared<K>(k);//
         std::cout << "push: " << k << " " << v << "\n";
         auto map_ptr = map_of_iterators;
         auto list_ptr = list_of_pairs;
-        bool was_unique = false;
-        if(map_of_iterators.use_count() > 2){
-            was_unique = true;
-            keyed_queue<K,V> new_queue(*this);
+        bool wasnt_unique = false;
+        if (map_of_iterators.use_count() > 2) {
+            wasnt_unique = true;
+            keyed_queue<K, V> new_queue(*this);
             new_queue.full_copy();//TODO moze byc podwojna kopia
             map_ptr = new_queue.map_of_iterators;
             list_ptr = new_queue.list_of_pairs;
         }
 
         auto found = map_ptr->find(k_ptr);//founs jest para (key,list<iterator>)
-        std::shared_ptr<V> v_ptr = std::make_shared<V>(v);//
+        std::shared_ptr <V> v_ptr = std::make_shared<V>(v);//
 
-        if(found != map_ptr->end()){//Jesli znalezlismy w mapie dany klucz
+        if (found != map_ptr->end()) {//Jesli znalezlismy w mapie dany klucz
             k_ptr = found->first;
-//            std::cout << "znal " << *k_ptr << " " << *(found->second.front())->second << "\n";
         }
 
-        std::list<pairKV> singleton({std::make_pair(k_ptr,v_ptr)});//
+        std::list <pairKV> singleton({std::make_pair(k_ptr, v_ptr)});//
+        std::list<itKV> singletonIt({singleton.begin()});//
 
         if (found == (map_ptr)->end()) {
-//            std::cout<<"powinno byc true:";
-            auto ret = (map_ptr)->insert(std::make_pair(k_ptr, list_of_itarator{}));//JEdyna funnkcja ktora moze wywalic i modyfikuje
-            if(ret.second == true){
-//                std::cout<<"jest\n";
+            auto ret = (map_ptr)->insert(
+                    std::make_pair(k_ptr, list_of_iterators{}));//JEdyna funnkcja ktora moze wywalic i modyfikuje
+            if (ret.second == true) {
                 found = ret.first;
-            } else{
-                //TODO
+            } else {
+                //TODO Czy insert moe rzucic wyjatek alokacji
             }
-        } else{
-//            for(auto i:*list_of_pairs){
-//                std::cout<<"("<<*i.first<<","<<*i.second<<") ";
-//            }
-//            std::cout<<"\n";
-            //              dodawane key i val      znalezione key         znaleziony klucz iteratora       znaleziony val iterator
-//                std::cout << k<<" "<<v<<" |  " << *found -> first << " "
-//                          <<  *(found->second.front()->first)<<"  "<<*(found->second.front()->second) << "             koniec\n";
         }
 
-        if(was_unique){
+        if (wasnt_unique) {
             map_of_iterators = map_ptr;
             list_of_pairs = list_ptr;
         }
 
-        (list_of_pairs)->push_back({k_ptr, v_ptr});//splice(list_of_pairs->end(),singleton);
-//        (list_of_pairs)->splice(list_of_pairs->end(),singleton);
-        auto help=list_of_pairs->end();
-        --help;
-        (found->second).push_back(help);
-//        std::cout << "Z tego klucza\n";
-//        for(auto i : found->second) {
-//            std::cout << *i->first << " " << *i->second << "**\n";
-//            auto b = i;
-//            ++b;
-//            if(b != list_of_pairs->end()) {
-//                std::cout<<"**\n";
-//                std::cout << *b->first << " " << *b->second << "<-nastepny\n";
-//            }
+        list_of_pairs->splice(list_of_pairs->end(),singleton);
+        found->second.splice((found->second).end(),singletonIt);
+//        auto it = list_of_pairs->begin();
+//        while(it != list_of_pairs->end()) {
+//            std::cout << *it->first << " " << *it->second << " | ";
+//            ++it;
 //        }
+//        std::cout << "\n";
     }
 
     void pop() {
@@ -173,61 +164,85 @@ public:
             throw lookup_error();
         }
         pop(*list_of_pairs->front().first);
-        pairKV p = list_of_pairs->front();
     }
 
     void pop(K const &k) {
         auto k_ptr = std::make_shared<K>(k);
-        auto found = map_of_iterators->find(k_ptr);//
+        auto map_ptr = map_of_iterators;
+        auto list_ptr = list_of_pairs;
+        bool wasnt_unique = false;
+        if (map_of_iterators.use_count() > 2) {
+            wasnt_unique = true;
+            keyed_queue<K, V> new_queue(*this);
+            new_queue.full_copy();//TODO moze byc podwojna kopia
+            map_ptr = new_queue.map_of_iterators;
+            list_ptr = new_queue.list_of_pairs;
+        }
+
+        auto found = map_ptr->find(k_ptr);//
+        if (found == map_ptr->end()) {
+            throw lookup_error();
+        }
+
+        if(wasnt_unique) {
+            map_of_iterators = map_ptr;
+            list_of_pairs = list_ptr;
+        }
+
+        itKV it = found->second.front();
+        ++it;
+        std::cout << *it->first << " " << *it->second << "\n";
+        list_of_pairs->erase(it);
+        std::cout << "*\n";
+        found->second.erase(found->second.begin());
+
+        if (found->second.empty()) {//TODO upewnic sie, ze nie rzuca
+            map_of_iterators->erase(k_ptr);
+        }
+        std::cout << "POP: " << k << "\n";
+        auto i = list_of_pairs->begin();
+        while(i != list_of_pairs->end()) {
+            std::cout << *i->first << " " << *i->second << " | ";
+            ++i;
+        }
+        std::cout << "\n";
+    }
+
+    void move_to_back(K const &k) {
+        auto k_ptr = std::make_shared<K>(k);
+        auto found = map_of_iterators->find(k_ptr);
         if (found == map_of_iterators->end()) {
             throw lookup_error();
         }
 
-        itKV it = found->second.front();
-       //
-      //  std::cout<<"++++++++++++++\n";
-      //  auto i = it;
-      //  while(i!=list_of_pairs->end()) {
-      //      std::cout << *i->first <<"|"<<*i->second<<"\n";
-      //      ++i;
-      //  }
-      //  std::cout<<"++++++++++++++\n";
+        std::list<pairKV> new_list_of_pairs;
+        std::list<itKV> new_list_of_iterators;
 
-        list_of_pairs->erase(it);
-      //   std::cout << "ZAWARTOSC PO USUNIECIU\n";
-      //  for(auto elem : *list_of_pairs) {
-      //      std::cout << *elem.first <<"|"<<*elem.second<<"\n";
-      //  }
-      //  std::cout<<"++++++++++++++\n";
-
-//        std::cout << found->
-        found->second.erase(found->second.begin());
-
-        if (found->second.empty()) {
-//            std::cout <<"*\n";
-            map_of_iterators->erase(k_ptr);
-        }
-//        std::cout <<"*\n";
-    }
-
-    void move_to_back(K const &k) {
-        auto found = map_of_iterators.find(k);
-        if (found == map_of_iterators.end()) {
-            throw lookup_error();
+        for(auto i = found->second.begin(); i!=found->second.end(); ++i) {
+            new_list_of_pairs.push_back(**i);
         }
 
-        list_of_itarator l = found->second;
-        int number_of_elements_to_move = l.size();
-
-        for (int i = 0; i < number_of_elements_to_move; i++) {
-            itKV it = l.front();
-            list_of_pairs.push_back(*it);
-            list_of_pairs.erase(it);
-            l.pop_front();
-            l.push_back(list_of_pairs.end()--);
+        for(auto i = new_list_of_pairs.begin(); i!=new_list_of_pairs.end(); ++i){
+            new_list_of_iterators.push_back(i);
+        }
+//        if(should_copy){}
+        auto new_queue = *this;
+        new_queue.full_copy();
+//Od teraz no-throw
+        while(found->second.empty() == false) {
+            list_of_pairs->erase(found->second.front());
+            found->second.pop_front();
         }
 
-        found->second = l;
+        list_of_pairs->splice(list_of_pairs->end(),new_list_of_pairs);
+        found->second.splice(found->second.end(),new_list_of_iterators);
+        std::cout << "MOVE TO BACK: " << k << "\n";
+        auto it = list_of_pairs->begin();
+        while(it != list_of_pairs->end()) {
+            std::cout << *it->first << " " << *it->second << " | ";
+            ++it;
+        }
+        std::cout << "\n";
     }
 
     std::pair<K const &, V &> front() {
--- pom.cpp
+++ pom.cpp
@@ -3,29 +3,36 @@
 #include "keyed_queue.h"
 using namespace std;
 int main() {
-  map<int, int> m;
-  keyed_queue<int, int> kolejka = {};
-  cout << kolejka.size() << "\n";
-  kolejka.push(100, 1);
-  kolejka.push(200, 1);
-  kolejka.push(200, 2);
-  kolejka.push(100, 2);
-//  kolejka.pop(200);
-  kolejka.pop();
-  kolejka.pop(200);
-  kolejka.pop(100);
-//  kolejka.pop(2);
-  cout <<" * z maina\n";
-  kolejka.push(100, 3);
-  //kolejka.pop();
-  cout << kolejka.size() << "\n";
-  for(auto b = kolejka.k_begin(); b != kolejka.k_end(); ++b)
-    cout << *b << " | ";
-  cout << "\n";
-  cout << kolejka.front().second << "\n";
-  cout << kolejka.back().second << "\n";
-  cout << kolejka.first(100).second << "\n";
-  cout << kolejka.last(200).second << "\n";
+    {
+        map<int, int> m;
+        keyed_queue<int, int> kolejka = {};
+        cout << kolejka.size() << "\n";
+        kolejka.push(100, 1);
+        kolejka.push(200, 1);
+        kolejka.push(200, 2);
+        kolejka.push(100, 2);
+//        kolejka.move_to_back(200);
+//        kolejka.pop();
+//        kolejka.pop(200);
+//        kolejka.pop(100);
+        cout << " * z maina\n";
+        keyed_queue kolejka1(kolejka);
+
+//        kolejka1.push(200, 3);
+//        kolejka.move_to_back(200);
+//        kolejka.push(123, 1);
+//        kolejka1.push(1, 1);
+        //kolejka.pop();
+//        cout << kolejka.size() << "\n";
+//        for (auto b = kolejka.k_begin(); b != kolejka.k_end(); ++b)
+//            cout << *b << " | ";
+
+        cout << "\n Teraz back\n";
+        kolejka1.pop();
+        kolejka.pop(200);
+        cout << kolejka.front().first << "\n";
+        cout << kolejka1.front().first << "\n";
+
 //  cout<<"kopiujemy\n";
 //  auto kolejka2(kolejka);
 //  kolejka.push(3,1);
@@ -35,18 +42,20 @@ int main() {
 //  for(auto b = kolejka.k_begin(); b != kolejka.k_end(); ++b)
 //    cout << *b << " | ";
 //  cout << "\n";
-  // //kolejka.front();
-  // auto a = kolejka.front();
-  // cout << a.first << " " << a.second << "\n";
-  // a.second = 123;
-  //
-  // auto b = kolejka.front();
-  // cout << b.first << " " << b.second << "\n";
-  //
-  // auto c = kolejka.first(14);
-  //   cout << c.first << " " << c.second << "\n";
-  // c.second = 1;
-  //
-  // kolejka.last(2).second = 15;
-  // cout << kolejka.back().second << "\n";
+        // //kolejka.front();
+        // auto a = kolejka.front();
+        // cout << a.first << " " << a.second << "\n";
+        // a.second = 123;
+        //
+        // auto b = kolejka.front();
+        // cout << b.first << " " << b.second << "\n";
+        //
+        // auto c = kolejka.first(14);
+        //   cout << c.first << " " << c.second << "\n";
+        // c.second = 1;
+        //
+        // kolejka.last(2).second = 15;
+        // cout << kolejka.back().second << "\n";
+    }
+
 }
